ddafdff74effb18b6498a2a84a7cb3b37ce3f0f4
jrbeverly <jrbeverly@live.com>
2020-01-10 18:56:17 -0500

    Install script for setting up docker environment

---
+++ .github/environments/dotnet-bazel/install.bash
@@ -0,0 +1,8 @@
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O /tmp/packages-microsoft-prod.deb
+dpkg -i /tmp/packages-microsoft-prod.deb
+
+add-apt-repository universe
+apt-get update
+apt-get install -y apt-transport-https
+apt-get update
+apt-get install -y dotnet-sdk-3.1
